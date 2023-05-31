1a8a88469d9d54b1e08731f3d69ca1f22197b21a
TanmayAggarwal07 <116791114+TanmayAggarwal07@users.noreply.github.com>
2023-05-31 15:55:22 +0530

    Add files via upload

---
+++ Heart.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+def heart_shape():
+    t = np.linspace(0, 2 * np.pi, 1000)
+    x = 16 * np.sin(t) ** 3
+    y = 13 * np.cos(t) - 5 * np.cos(2 * t) - 2 * np.cos(3 * t) - np.cos(4 * t)
+
+    return x, y
+
+x, y = heart_shape()
+
+st.title("I LOVE YOU BBY AND MISS YOU")
+
+fig, ax = plt.subplots()
+ax.plot(x, y, color='red')
+ax.fill(x, y, color='red')
+ax.set_aspect("equal")
+ax.axis("off")
+
+st.pyplot(fig)
